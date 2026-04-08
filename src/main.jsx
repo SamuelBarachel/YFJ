4a17fc11f8babd372eb1be2690fe3495c1114d06
Samuel Barachel <takwirira@proton.me>
2026-04-07 23:03:18 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AuthProvider } from './context/AuthContext'
-import './assets/index.css'
+import './assets/index.css' // Ensure the folder is named 'assets' (lowercase)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
